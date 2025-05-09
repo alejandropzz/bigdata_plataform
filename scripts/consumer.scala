0bb9a9f7dda3bba13428d8962347d4328d02b75f
alejandropzz <amlh26c7@gmail.com>
2025-05-09 01:57:52 -0600

    SE COMPLETO YARN, HDFS, SPARK, KAFKA, DEBEZIUM, CONFLUENT

--- scripts/consumer.scala
+++ scripts/consumer.scala
@@ -1,23 +1,81 @@
-// 1. Objeto principal (US-ASCII ONLY)
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+
 object KafkaConsumer {
   def main(args: Array[String]): Unit = {
-    // 2. Configuracion directa
+    
     val df = spark
       .readStream
       .format("kafka")
-      .option("kafka.bootstrap.servers", "kafka:9092")
-      .option("subscribe", "cdc")
-      .option("startingOffsets", "earliest")
+      .option("kafka.bootstrap.servers", "broker:29092")
+      .option("subscribe", "cdc.public.transactions")
+      .option("startingOffsets", "latest")
       .load()
 
-    // 3. Procesamiento 
-    df.selectExpr("CAST(value AS STRING) as message")
-      .writeStream
-      .format("console")
+
+    val transactionPayloadSchema = new StructType()
+      .add("before", new StructType()
+        .add("transaction_id", StringType)
+        .add("user_id", StringType)
+        .add("timestamp", LongType)
+        .add("amount", StringType)
+        .add("city", StringType)
+        .add("country", StringType)
+        .add("merchant_name", StringType)
+        .add("payment_method", StringType)
+        .add("affiliateid", StringType)
+      )
+      .add("after", new StructType()
+        .add("transaction_id", StringType)
+        .add("user_id", StringType)
+        .add("timestamp", LongType)
+        .add("amount", StringType)
+        .add("city", StringType)
+        .add("country", StringType)
+        .add("merchant_name", StringType)
+        .add("payment_method", StringType)
+        .add("affiliateid", StringType)
+      )
+      .add("op", StringType)
+      .add("ts_ms", LongType)
+
+    val envelopeSchema = new StructType()
+      .add("payload", transactionPayloadSchema)
+
+
+    val parsedDF = df
+      .selectExpr("CAST(value AS STRING) as message")
+      .withColumn("jsonData", from_json(col("message"), envelopeSchema))
+      .select("jsonData.payload.*")
+
+    val dfWithPartitioning = parsedDF
+      .withColumn("year", year(from_unixtime(col("ts_ms") / 1000)))
+      .withColumn("month", month(from_unixtime(col("ts_ms") / 1000)))
+      .withColumn("day", dayofmonth(from_unixtime(col("ts_ms") / 1000)))
+      .withColumn("hour", hour(from_unixtime(col("ts_ms") / 1000)))
+      .withColumn("minute", minute(from_unixtime(col("ts_ms") / 1000)))
+
+    dfWithPartitioning.writeStream
+      .format("parquet")  
+      .option("checkpointLocation", "hdfs://namenode:9000/user/bigdata_plataform/cdc/transactions/_checkpoint")
+      .option("path", "hdfs://namenode:9000/user/bigdata_plataform/cdc/transactions")
+      .partitionBy("year", "month", "day", "hour", "minute")
       .start()
       .awaitTermination()
+
+/*
+    dfWithPartitioning.writeStream
+      .format("console")
+      .option("truncate", false)
+      .start()
+      .awaitTermination()
+*/
+  
   }
 }
 
-// 4. Llamada  (requerida en spark-shell)
 KafkaConsumer.main(Array())
+
+
